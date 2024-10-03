66b02940f36279392a09e7a8a757ff4371dbb4dd
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-10-03 14:51:16 +0100

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,22 @@
+
+//task 1
+let userName = "";
+
+//task2
+if (userName === ''){
+  console.log("Hello" + userName + '!')
+} else {
+console.log ("Hello")
+}
+
+//task3
+const userQuestion = "";
+
+//task4
+console.log(userName + userQuestion);
+
+//task5
+const randomNumber = Math.floor(Math.random()* 8);
+
+//task6
+const eightBall = "";
