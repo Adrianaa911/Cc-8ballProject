e43e7c1994dc40e66e2c3d665ce0bf5a0051d5d9
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-10-04 10:17:42 +0100

    Step_by_step-project

--- Code.js
+++ Code.js
@@ -1,24 +1,43 @@
+//task 1 In the first line of the program, define a variable called userName that is set to an empty string.
 
-//task 1
-let userName = "";
+let userName = "Adriana";
 
-//task2
+//task2 Below this variable, create a ternary expression that decides what to do if the user enters a name or not. If the user enters a name — like 'Jane' — use string interpolation to log Hello, Jane! to the console. Otherwise, simply log Hello!.
+
 userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
-//if the userName has a string(name) will print 'Hello -name-', if not will print 'Hello!'
 
-//task 3
+//task3 Create a variable named userQuestion. The value of the variable should be a string that is the question the user wants to ask the Magic Eight Ball.
+
 let userQuestion = "Will I have a good day today?";
 
-//task4
+//task4 Write a console.log() for the userQuestion, stating what was asked. You can include the user’s name in the console.log() statement, if you wish!
+
 console.log(`${userName} has asked - ${userQuestion}`);
 
-//task5
+//task5 We need to generate a random number between 0 and 7.Create another variable, and name it randomNumber. Set it equal to this expression, which uses two methods (Math.floor() and Math.random()) from the Math library.
+//Math.floor(Math.random() * 8);
+
 let randomNumber = Math.floor(Math.random ()* 8);
 console.log(randomNumber);
-//task6
+
+//task6 Create one more variable named eightBall, and set it equal to an empty string. We will save a value to this variable in the next steps, depending on the value of randomNumber
+
 let eightBall = '';
 
-// task 7
+// task 7 
+/*
+We need to create a control flow that takes in the randomNumber we made in step 5, and then assigns eightBall to a reply that a Magic Eight Ball would return. Think about utilizing if/else or switch statements. Here are 8 Magic Eight Ball phrases that we’d like to save to the variable eightBall:
+
+'It is certain'
+'It is decidedly so'
+'Reply hazy try again'
+'Cannot predict now'
+'Do not count on it'
+'My sources say no'
+'Outlook not so good'
+'Signs point to yes'
+If the randomNumber is 0, then save an answer to the eightBall variable; if randomNumber is 1, then save the next answer, and so on. If you’re feeling creative, make your own responses!
+*/
 switch(randomNumber) {
   case 0:
   eightBall = 'It is certain';
@@ -46,20 +65,8 @@ switch(randomNumber) {
   break;
 }
 
-  console.log(`The Magic 8 Ball says, ${eightBall}.`);
-
-
  //task 8 - Write a console.log() to print the Magic Eight Ball’s answer, the value of the eightBall variable
   console.log(`The Magic 8 Ball says, ${eightBall}.`);
 
   //task 9 Run your program a few times to see random results appear in the console!
- 
-
-  
-
-
-
-
-
-
  
